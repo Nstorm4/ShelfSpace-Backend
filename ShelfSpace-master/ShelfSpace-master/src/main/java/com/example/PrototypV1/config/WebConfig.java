7e4bf17378ae7477c9c6da64ba6f19e9e3402de3
freinwand <reinwandfranz@gmail.com>
2024-10-22 19:24:16 +0200

    Lets go

--- ShelfSpace-master/ShelfSpace-master/src/main/java/com/example/PrototypV1/config/WebConfig.java
+++ ShelfSpace-master/ShelfSpace-master/src/main/java/com/example/PrototypV1/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://shelfspace-react.apps.01.cf.eu01.stackit.cloud")
+                .allowedOrigins("https://shelfspace-react.apps.01.cf.eu01.stackit.cloud")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
